923b4b000216ce7079397b5753df4d0766ec9315
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-06-15 12:47:37 +0530

    Added routes to app.jsx file for notes screens and added css codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+// import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Notes from "./screens/Notes"
+import CreateNote from "./screens/CreateNote"
+import EditNote from "./screens/EditNote"
 
 const App = () => {
     return (
-        <div>App</div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Notes />} />
+                <Route path="/create-note" element={<CreateNote />} />
+                <Route path="/edit-note/:id" element={<EditNote />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
