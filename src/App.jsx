3c20f6663fc5dcc75e43171e650ba3dbdad23f66
Nirdhum <66917558+nirdhum@users.noreply.github.com>
2023-06-15 18:18:46 +0530

    Made changes to app.jsx and index.html file

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Notes from "./screens/Notes"
 import CreateNote from "./screens/CreateNote"
 import EditNote from "./screens/EditNote"
+import { useState } from "react"
+
+// import dummyNotes from './dummy-notes'
 
 const App = () => {
+
+    const [notes, setNotes] = useState([])
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Notes />} />
-                <Route path="/create-note" element={<CreateNote />} />
-                <Route path="/edit-note/:id" element={<EditNote />} />
-            </Routes>
-        </BrowserRouter>
+        <main id="app">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Notes notes={notes} />} />
+                    <Route path="/create-note" element={<CreateNote setNotes={setNotes} />} />
+                    <Route path="/edit-note/:id" element={<EditNote />} />
+                </Routes>
+            </BrowserRouter>
+        </main>
     )
 }
 
